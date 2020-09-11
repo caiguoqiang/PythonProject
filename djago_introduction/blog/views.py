47aa06e541d79c8e0585391a9e416f87b09a480d
cai <1144395740@qq.com>
2020-09-11 16:00:11 +0800

    完成分页及查询最新文章展示，

--- djago_introduction/blog/views.py
+++ djago_introduction/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Article
+from django.core.paginator import Paginator
 # Create your views here.
 def hello_world(request):
     return HttpResponse("helloworld")
@@ -18,15 +19,41 @@ def Article_content(request):
     return HttpResponse(return_str)
 
 def get_index_page(request):
+    page = request.GET.get('page')
+    if page:
+        page = int(page)
+    else:
+        page = 1
+    #print('page parmar',page)
     all_artcle = Article.objects.all()
+    top5_artcle_list = Article.objects.order_by('publish_date')[:4]
+    paginator = Paginator(all_artcle,5)
+    page_num = paginator.num_pages
+    #print('page num',page_num)
+    page_artcle_list = paginator.page(page)
+    if page_artcle_list.has_next():
+        next_page = page+1
+    else:
+        next_page = page
+    if page_artcle_list.has_previous():
+        previous_page = page-1
+    else:
+        previous_page = page
+
     return render(request,'blog/index.html',
                   {
-                      'artcle_list':all_artcle
+                      'artcle_list':page_artcle_list,
+                      'page_num':range(1,page_num+1),
+                      'curr_page':page,
+                      'next_page':next_page,
+                      'previous_page':previous_page,
+                      'top5_artcle_list':top5_artcle_list,
                   }
                   )
 
 def get_detail_page(request,artcle_id):
     all_artcle = Article.objects.all()
+    #print(type(all_artcle))
     curr_artcle = None
     previous_index = 0
     next_index = 0
@@ -56,5 +83,6 @@ def get_detail_page(request,artcle_id):
                       'section_list':section_list,
                       'previous_artcle':previous_artcle,
                       'next_artcle':next_artcle,
+
                   }
                   )
